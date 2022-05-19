91add4230c002c8eba9ad0f46408960904f498af
Raoji Patil <raojipatil22@gmail.com>
2022-05-19 19:05:52 +0530

    Initial commit

---
+++ hooks.php
@@ -0,0 +1,48 @@
+<?php 
+
+
+// Filter hooks type:- 
+
+// Add_filter()
+// apply_filter()
+// remove_filter()
+// current_filter()
+// has_filter()
+// remove_all_filter()
+// doing_filter()
+
+
+?>
+
+
+<?php
+// action hooks
+add_action( 'footer_scripts', 'custom_footer_scripts' );
+function footer_scripts(){
+
+$score = 100;
+echo "Current score is : ". apply_filters( 'change_score', $score );
+}
+
+
+?>
+
+<?php 
+// filter hooks
+// WordPress offers filter hooks to allow plugins to modify various types of internal data at runtime.
+
+add_filter( 'change_score', 'function_change_score' );
+function function_change_score( $score ){
+ $score+=100;
+ return $score;
+}
+?>
+
+<?php
+function example_callback( $example ) {
+    // Maybe modify $example in some way.
+    return $example;
+}
+add_filter( 'example_filter', 'example_callback' );
+
+?>
